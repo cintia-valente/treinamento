acb14b7c99f65c68ce5869a5c22c500ff874149e
Cíntia <38113015+cintia-valente@users.noreply.github.com>
2026-02-20 15:37:34 -0300

    Add files via upload

---
+++ Pxcw00xn_ProcessaTelaHtml.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Services;
+using Bergs.Pwx.Pwxoiexn.IN;
+
+namespace Bergs.Pxc.Pxcw00xn
+{
+    /// <summary>Classe responsável pelo processamento das telas da aplicação.</summary>
+    [WebService(Namespace = "Bergs.Pxc.Pxcw00xn")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    public class ProcessaTelaHtml : AplicacaoProcessaTelaHtml
+    {
+        #region Métodos
+        #endregion
+    }
+}
+ 
